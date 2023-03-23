18dd1ff78d816c9d1c2a4f2fb4d69bdb7c433797
Katock <89233261+Katock-Cricket@users.noreply.github.com>
2023-03-23 23:46:05 +0800

    de

--- RequirementsModel/src-gen/entity/entity.go
+++ RequirementsModel/src-gen/entity/entity.go
@@ -2,31 +2,31 @@ package entity
 import "time"
 
 type Item struct {
-	Barcode Int64 `db:"barcode"`
-	Name String `db:"name"`
-	Price Float64 `db:"price"`
-	StockNumber Int64 `db:"stock_number"`
-	Description String `db:"description"`
-	BelongedShopShopId Int64 `db:"belonged_shop_shop_id"`
+	Barcode int64 `db:"barcode"`
+	Name string `db:"name"`
+	Price float64 `db:"price"`
+	StockNumber int64 `db:"stock_number"`
+	Description string `db:"description"`
+	BelongedShopShopId int64 `db:"belonged_shop_shop_id"`
 }
 type User struct {
-	UserId String `db:"user_id"`
-	Username String `db:"username"`
+	UserId string `db:"user_id"`
+	Username string `db:"username"`
 }
 type Sale struct {
-	GenerateId Int64 `db:"generate_id"`
-	Number Int64 `db:"number"`
-	Price Float64 `db:"price"`
-	ItemBarcode Int64 `db:"item_barcode"`
-	BelongedOrderOrderId Int64 `db:"belonged_order_order_id"`
+	GenerateId int64 `db:"generate_id"`
+	Number int64 `db:"number"`
+	Price float64 `db:"price"`
+	ItemBarcode int64 `db:"item_barcode"`
+	BelongedOrderOrderId int64 `db:"belonged_order_order_id"`
 }
 type Order struct {
-	OrderId Int64 `db:"order_id"`
-	TotalPrice Float64 `db:"total_price"`
-	IsPayed Bool `db:"is_payed"`
-	BelongedUserUserId String `db:"belonged_user_user_id"`
+	OrderId int64 `db:"order_id"`
+	TotalPrice float64 `db:"total_price"`
+	IsPayed bool `db:"is_payed"`
+	BelongedUserUserId string `db:"belonged_user_user_id"`
 }
 type Shop struct {
-	ShopId Int64 `db:"shop_id"`
-	ShopName String `db:"shop_name"`
+	ShopId int64 `db:"shop_id"`
+	ShopName string `db:"shop_name"`
 }
